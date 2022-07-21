3b490b4c96490eedbc6a4638acae0b07e7f6e578
tetrapacker238 <alejandro.salas@live.cl>
2022-07-21 18:02:29 -0400

    added ferris wheel solution

--- Sorting and Searching/ferrisWheel.cpp
+++ Sorting and Searching/ferrisWheel.cpp
@@ -2,30 +2,43 @@
 using namespace std; 
 #define SPEED ios::sync_with_stdio(false); cin.tie(0); cout.tie(0) 
 #define ll long long 
+#define ull unsigned long long
 #define ld long double 
 #define le length
 #define rep(i , j , n) for(ll i = j ; i <= n ; i++) 
 #define per(i , j , n) for(ll i = j ; i >= n ; i--)  
-
-
 
 int main() { 
     SPEED; 
     cout.precision(8); 
     cout << fixed;
     
-    ll n, x;
+    ull n, x, counter = 0;
     cin >> n >> x;
-    vector<vector<ll>> children(n, vector<ll>(2, 0));
+    vector<ull> children(n);
     for (ll i = 0; i < n; i++) {
-        cin >> children[i][0];
+        cin >> children[i];
     }
 
     sort(children.begin(), children.end());
 
-    for (ll i = 0; i < n; i++) {
-        
+    ull l = 0, r = n - 1;
+
+    for(;l<r;) {
+        counter++;
+        if (children[l] + children[r] > x) { //Caso descartamos el r
+            r--;
+        } else { //messirve, tomamos ambos extremos
+            l++;
+            r--;
+        }
     }
+
+    if (l == r) {
+        counter++;
+    }
+
+    cout << counter;
 
     return 0; 
 } 
