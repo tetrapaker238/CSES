35ba0c784ff3f06630db4f823a4212f67e97b7b2
tetrapacker238 <alejandro.salas@live.cl>
2022-07-23 13:36:50 -0400

    added concert tickets solution with sets

--- Sorting and Searching/concertTickets.cpp
+++ Sorting and Searching/concertTickets.cpp
@@ -1,40 +1,63 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-#define SPEED ios::sync_with_stdio(false); cin.tie(0); cout.tie(0) 
-#define ll long long 
-#define ld long double 
+#include <bits/stdc++.h>
+using namespace std;
+#define SPEED ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define ll long long
+#define ull unsigned long long
+#define ld long double
 #define ull unsigned long long
 #define le length
-#define rep(i , j , n) for(ll i = j ; i <= n ; i++) 
-#define per(i , j , n) for(ll i = j ; i >= n ; i--)  
+#define rep(i , j , n) for(ll i = j ; i <= n ; i++)
+#define per(i , j , n) for(ll i = j ; i >= n ; i--)
 
-
-
-int main() { 
-    SPEED; 
-    cout.precision(8); 
+int main() {
+    SPEED;
+    cout.precision(8);
     cout << fixed;
-    ll n = 0, m = 0, maxPrice = 0, pos = 0;
-    vector<ll>::iterator low;
+    ull n, m, val;
     cin >> n >> m;
-    vector<vector<ll>> tickets(n, vector<ll>(2, 0));
-    for (ll i = 0; i < n; i++) {
-        cin >> tickets[i][0];
+    set<vector<ull>> s;
+    vector<ull> vtf(2, 0);
+    for (ull i = 0; i < n; i++) {
+        cin >> val;
+        s.insert({val, i});
     }
 
-    sort(tickets.begin(), tickets.end());
-
-    for (ll i = 0; i < m; i++) {
-        cin >> maxPrice;
-        low = lower_bound(tickets.begin(), tickets.end(), maxPrice);
-        pos = low - tickets.begin();
-        if (pos >= 0 && (tickets[pos] <= maxPrice)) {
-            cout << tickets[pos] << "\n";
-        } else if (pos > 0 && (tickets[pos - 1] <= maxPrice)){
-            cout << tickets[pos - 1] << "\n";
-        } else {
+    for (ull i = 0; i < m; i++) {
+        cin >> vtf[0];
+        if (s.empty()) {
             cout << -1 << "\n";
+            continue;
+        }
+
+        auto it = s.upper_bound(vtf);
+        if ((it == s.begin())) {
+            if ((*it)[0] <= vtf[0]) {
+                cout << (*it)[0] << "\n"; 
+                s.erase(it);
+            } else {
+                cout << -1 << "\n";
+            }
+        } else if (it != s.end()) {
+            auto prevIt = prev(it, 1);
+
+            if ((*it)[0] <= vtf[0]) {
+                cout << (*it)[0] << "\n"; 
+                s.erase(it);
+            } else if (((*prevIt)[0] <= vtf[0])) {
+                cout << (*prevIt)[0] << "\n"; 
+                s.erase(prevIt);
+            } else {
+                cout << -1 << "\n";
+            }
+        } else {
+            auto prevIt = prev(it, 1);
+            if ((*prevIt)[0] <= vtf[0]) {
+                cout << (*prevIt)[0] << "\n"; 
+                s.erase(prevIt);
+            } else {
+                cout << -1 << "\n";
+            }
         }
     }
-    return 0; 
-} 
+    return 0;
+}
