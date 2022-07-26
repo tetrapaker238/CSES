6c8f32498974ad863101f7e329d8ec25034fb9f1
tetrapacker238 <alejandro.salas@live.cl>
2022-07-26 16:49:59 -0400

    added some files

---
+++ Sorting and Searching/missingCoinSum.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+#define SPEED ios::sync_with_stdio(false); cin.tie(0); cout.tie(0) 
+#define ll long long 
+#define ld long double 
+#define ull unsigned long long
+#define le length
+#define rep(i , j , n) for(ll i = j ; i <= n ; i++) 
+#define per(i , j , n) for(ll i = j ; i >= n ; i--)  
+
+int main() { 
+    SPEED; 
+    cout.precision(8); 
+    cout << fixed;
+    ll n;
+    cin >> n;
+
+    vector<ll> coins(n);
+    for(ll i = 0; i < n; i++) {
+        cin >> coins[i];
+    }
+    return 0; 
+} 
