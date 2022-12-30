4b34fa3fffc4d694e6d4d93bc4786dec6538b980
Alejandro <alejandro.salas@live.cl>
2022-12-30 13:32:34 -0300

    added some thoughts on Dynamic Programming

--- Dynamic Programming/minimizingCoins.cpp
+++ Dynamic Programming/minimizingCoins.cpp
@@ -12,6 +12,43 @@ int main() {
     SPEED; 
     cout.precision(8); 
     cout << fixed;
- 
+
+    ll n, x;
+
+    cin >> n >> x;
+
+    vector<ll> c(n);
+
+    rep(i, 0, n - 1) {
+        cin >> c[i];
+    }
+
+    vector<ll> dp(x + 1, 0);
+
+    per(i, n - 1, 0) {
+        ll j = c[i];
+        ll counter = 1;
+        while(j <= x) {
+            if (dp[j] == 0) { // Todavía no hay una mejor combinación para esta moneda
+                dp[j] = counter;
+            } else { // Debemos ver si la combinación actual tiene menos valor que la anterior
+                if (dp[j] > counter) {
+                    dp[j] = counter;
+                }
+            }
+            counter++;
+            j+= c[i];
+        }
+    }
+
+    //Ahora hay que hacer dp[i] = min(dp[i], dp[i - 1] + dp[1])
+    //Pero notar que solo nos interesa dp[x], y por tanto, solo va a interesar cuanto nos falte para ese número y quizás se puede hacer
+    //recursivo hacia atrás para obtener los valores que nos interesan (o iterativo) calculando los valores primitivos para luego llegar al pedido
+    // bottom up 
+
+    rep(i, 0, x - 1) {
+        cout << dp[i] << "\n";
+    }
+
     return 0; 
 } 
